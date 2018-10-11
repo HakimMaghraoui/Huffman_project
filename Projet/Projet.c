93a71239399d83196414208de1c50c68af747630
Hak97 <36701735+Hak97@users.noreply.github.com>
2018-10-11 18:25:00 +0200

    Add files via upload

--- Projet/Projet.c
+++ Projet/Projet.c
@@ -1,98 +1,47 @@
 #include "Node.h"
+#include <stdio.h>
 
 int main(){
-	
-
-
-	pinfo inf=create_info(7,'o');	
-	pinfo infl=create_info(700,'o');
-	pinfo infr=create_info(70,'o');
-	// struct info infl=create_info(90,"p");
-
-	// struct info infr=create_info(87,"i");
-
-	 pnode n=create_node(inf);
-	// Show_node(&n);
-	 // delete_info(inf);
-	 // Show_node(&n);
-	 pnode nl= create_node(infl);
-	 pnode nr= create_node(infr);
-	//delete_info(inf); 
-
-	//Show_node(&n);
-	// Show_node(&n);
-	//  pnode nl= create_node(infl);
-	//  pnode nr= create_node(infr);
-
-	// add_left(&n,&nl);
-	// add_right(&n,&nr);
-	// Show_node(&n);
-	// delete_node(nl);
-	// Show_node(&n);
-	pnode n2=create_node(inf);
-	pnode nl2= create_node(infl);
-	pnode nr2= create_node(infr);
-	 add_left(&n,&nl);
-	 add_right(&n,&nr);
-	//Show_node(&n);
-
-	
-	ptree pt=create_tree(n);
-	// Show_node(&(*pt).root);
-	// delete_tree(&pt);
-	// Show_node(&(*pt).root);
-
-	printf("%s\n","test fusion" );
-	ptree pt2=create_tree(n2);
-	 ptree p_f=fusion(pt,pt2);
-	 Show_node(&(*p_f).root);
-	 pnode p_f_n=(*p_f).root;
-	 printf("%s\n","///////" );
-	
-	li liste=createListe();
-	char* texte= "hello";
-	liste=symbole_frequency(texte);
-	nodeL tmp=liste->head;
-	while(tmp!=NULL){
-		printf("%c",tmp->val->root->in->symbole);
-		printf("%d\n",tmp->val->root->in->frequency);
-		tmp=tmp->suivant;
+	int run=1;
+	int k;
+	char* texte=(char*)malloc(sizeof(char));
+	while(run){
+		printf("MENU\n");
+		printf("1. Compression d'un texte depuis l'entrée standard vers la sortie standard\n");
+		printf("2. Compression d'un texte depuis un fichier .txt dans un fichier .txt\n");
+		printf("3. Quit\n");
+		scanf("%d",&k);
+		switch(k){
+			case 1:
+				printf("Veuillez entrer votre texte(no spaces):   ");
+				scanf("%s",texte);
+				li liste= symbole_frequency(texte);
+				ptree bigtree=big_tree(liste);
+				char* code= compress(texte,bigtree);
+				printf("Code: %s\n",code);
+				int kk=3;
+				while(kk<1 || kk>2){
+					printf("Voulez vous le decompresser ou non? :\n");	
+					printf("1. Oui\n");
+					printf("2. Non\n");
+					scanf("%d",&kk);
+				}
+				if(kk==1){
+					char* code_decompress=uncompress(code,bigtree);
+					printf("%s\n",code_decompress);
+				}
+				break;
+			case 2:
+				printf("2\n");
+				break;
+			case 3:
+				run=0;
+				break;
+			default:
+				printf("S'il vous plait, veuillez entrer un entier valide\n");
+				break;
+		}		
 	}
-	printf("\n");
-	pinfo infaa=create_info(2,'j');
-	pnode aa=create_node(infaa);
-	ptree taa=create_tree(aa);
-	//delete_node_liste(liste, taa);
-	tmp=liste->head;
-	while(tmp!=NULL){
-		printf("%c",tmp->val->root->in->symbole);
-		printf("%d\n",tmp->val->root->in->frequency);
-		tmp=tmp->suivant;
-	}
-	printf("\n");
-	ptree tbb=min_freq_liste(liste);
-	printf("%c",tbb->root->in->symbole);
-	printf("%d\n",tbb->root->in->frequency);
-	
-
-	printf("%s\n","big tree test:" );
-	ptree btree=big_tree(liste);
-	printf("%s\n","test sd" );
-	tmp=liste->head;
-	while(tmp!=NULL){
-		printf("%c",tmp->val->root->in->symbole);
-		printf("%d\n",tmp->val->root->in->frequency);
-		tmp=tmp->suivant;
-	}
-	char* code=(char*)malloc(sizeof(char));
-	code=compress(texte,btree);
-	printf("%s\n",code);
-	char* uncode=uncompress(code,btree);
-	printf("%s\n",uncode);
-	
-
-	///////////////////////////////
-	printf("%s\n","complet" );
 	return 0;
 }
 
